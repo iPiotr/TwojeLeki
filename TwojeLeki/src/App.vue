c3bfa846ba77d8cc96dc7b848e9d623a63051f09
Piotr Starzec <ip.starzec@gmail.com>
2026-02-21 01:51:43 +0100

    [#1]  Folder arrangement, tailwind setup

---
+++ TwojeLeki/src/App.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+  <div class="bg-gray-800 flex justify-center h-screen">
+    <h1 class="text-3xl font-bold text-center text-red-800">TwojeLeki</h1>
+  </div>
+</template>
